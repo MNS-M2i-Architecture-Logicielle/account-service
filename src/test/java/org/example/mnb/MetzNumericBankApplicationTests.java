87cc7b4e6ec035c49fff4f443d2a52690b3c82ca
thomas <tomceg@hotmail.fr>
2025-06-18 11:37:52 +0200

    add test WIP

--- src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
+++ src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
@@ -1,6 +1,5 @@
 package org.example.mnb;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +9,5 @@ class MetzNumericBankApplicationTests {
     @Test
     void contextLoads() {
     }
+
 }
