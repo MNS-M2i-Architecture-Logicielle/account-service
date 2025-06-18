b4d6e9401238dd1b1b4a18bb262642816e177c13
thomas <tomceg@hotmail.fr>
2025-06-18 11:25:11 +0200

    first commit

---
+++ src/main/java/org/example/mnb/MetzNumericBankApplication.java
@@ -0,0 +1,13 @@
+package org.example.mnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetzNumericBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetzNumericBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetzNumericBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
