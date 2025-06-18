0c9c0ac7f905d010938452379a1f27b80d3e9f64
thomas <tomceg@hotmail.fr>
2025-06-18 11:37:52 +0200

    add test WIP

--- src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
+++ src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
@@ -1,5 +1,6 @@
 package org.example.mnb;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class MetzNumericBankApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void getCount_returnIsRight(String number) {
+        Assertions.assertEquals("3000000", number );
     }
 
 }
