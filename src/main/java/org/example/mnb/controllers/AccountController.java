47c81dd8bca80b0093d2a6df2d96105c95aa57c0
Layp <frau.nicolas7@gmail.com>
2025-06-18 15:49:51 +0200

    Added DTOs for Account & Client Added Client not found exception

--- src/main/java/org/example/mnb/controllers/AccountController.java
+++ src/main/java/org/example/mnb/controllers/AccountController.java
@@ -1,5 +1,6 @@
 package org.example.mnb.controllers;
 
+import org.example.mnb.dtos.AccountDTO;
 import org.example.mnb.entities.Account;
 import org.example.mnb.services.AccountService;
 import org.springframework.web.bind.annotation.*;
@@ -27,8 +28,9 @@ public class AccountController {
     }
     
     @PostMapping("/new")
-    public void createAccount(@RequestBody Account account){
+    public String createAccount(@RequestBody AccountDTO account){
         accountService.createAccount(account);
+        return "Account successfully created !";
     }
     
     @GetMapping("/balance/{id}")
