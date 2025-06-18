49cfd274177c7826bb366bbb4862df01d60be422
thomas <tomceg@hotmail.fr>
2025-06-18 11:44:18 +0200

    modify branche name

--- src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
+++ src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
@@ -1,6 +1,5 @@
 package org.example.mnb;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,11 +8,6 @@ class MetzNumericBankApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void getCount_returnIsRight(String number) {
-        Assertions.assertEquals("3000000", number );
     }
 
 }
