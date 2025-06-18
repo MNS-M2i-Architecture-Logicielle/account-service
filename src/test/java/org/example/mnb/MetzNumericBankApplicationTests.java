8d314439b989961f3071854804be74abe707d38d
thomas <tomceg@hotmail.fr>
2025-06-18 15:52:01 +0200

    modify setter getter add mockito dependency add unit tests for AccountService

--- src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
+++ src/test/java/org/example/mnb/MetzNumericBankApplicationTests.java
@@ -10,10 +10,4 @@ class MetzNumericBankApplicationTests {
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void getCount_returnIsRight(String number) {
-        Assertions.assertEquals("3000000", number );
-    }
-
 }
